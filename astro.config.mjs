18b9eeb83862a215a405a721cabde26721f43780
saikaitastuki <bed23018@uhe.ac.jp>
2026-09-22 15:26:27 +0900

    add GitHub Pages deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://danielcycling.github.io/daniel',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
